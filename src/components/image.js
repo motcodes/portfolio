fb18d99e1ae28fdd4d7a548a27104b3203ba54ee
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2020-02-15 14:41:07 +0100

    changed to logo.png file

--- src/components/image.js
+++ src/components/image.js
@@ -1,24 +1,13 @@
-import React from "react"
-import { useStaticQuery, graphql } from "gatsby"
-import Img from "gatsby-image"
+import React from 'react'
+import { useStaticQuery, graphql } from 'gatsby'
+import Img from 'gatsby-image'
 
-/*
- * This component is built using `gatsby-image` to automatically serve optimized
- * images with lazy loading and reduced file sizes. The image is loaded using a
- * `useStaticQuery`, which allows us to load the image from directly within this
- * component, rather than having to pass the image data down from pages.
- *
- * For more information, see the docs:
- * - `gatsby-image`: https://gatsby.dev/gatsby-image
- * - `useStaticQuery`: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
-const Image = () => {
+const Logo = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "Unsplash_Symbol.png" }) {
+      placeholderImage: file(relativePath: { eq: "Logo.png" }) {
         childImageSharp {
-          fluid(maxWidth: 100) {
+          fluid(maxWidth: 96) {
             ...GatsbyImageSharpFluid
           }
         }
@@ -26,7 +15,12 @@ const Image = () => {
     }
   `)
 
-  return <Img fluid={data.placeholderImage.childImageSharp.fluid} />
+  return (
+    <Img
+      fluid={data.placeholderImage.childImageSharp.fluid}
+      style={{ width: '96px' }}
+    />
+  )
 }
 
-export default Image
+export { Logo }

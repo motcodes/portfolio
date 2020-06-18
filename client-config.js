42ec4ec7cd1336473d307d29ca2d74cfd7d2c0d5
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2020-06-18 18:25:38 +0200

    ha

--- client-config.js
+++ client-config.js
@@ -5,6 +5,6 @@ require('dotenv').config({
 module.exports = {
   sanity: {
     projectId: process.env.GATSBY_SANITY_PROJECT_ID,
-    dataset: process.env.GATSBY_SANITY_DATASET,
+    dataset: 'production',
   },
 }

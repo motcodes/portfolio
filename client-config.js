0c2a34d646ca57691e39897d47504b0223c48b6c
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2020-06-18 18:45:30 +0200

    ha

--- client-config.js
+++ client-config.js
@@ -1,10 +1,6 @@
-require('dotenv').config({
-  path: `.env.${process.env.NODE_ENV}`,
-})
-
 module.exports = {
   sanity: {
     projectId: process.env.SANITY_PROJECT_ID,
-    dataset: 'production',
+    dataset: process.env.SANITY_PROJECT_DATASET || 'production',
   },
 }

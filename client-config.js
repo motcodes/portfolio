4beded77b1baf2e51051be6a2eae0fabfd1cd684
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2020-06-18 18:48:11 +0200

    ha

--- client-config.js
+++ client-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   sanity: {
-    projectId: process.env.SANITY_PROJECT_ID,
+    projectId: process.env.SANITY_PROJECT_ID || 'qhdnmhc0',
     dataset: process.env.SANITY_PROJECT_DATASET || 'production',
   },
 }

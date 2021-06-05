dc3b1392a03b91c513c9e0bc53f4a26cd145d5d4
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2021-06-05 15:28:07 +0200

    minor changes

--- src/components/helpers/portableText.js
+++ src/components/helpers/portableText.js
@@ -3,24 +3,28 @@ import clientConfig from '../../../client-config'
 import BasePortableText from '@sanity/block-content-to-react'
 import imageUrlBuilder from '@sanity/image-url'
 
-// const builder = imageUrlBuilder(clientConfig.sanity)
+const builder = imageUrlBuilder(clientConfig.sanity)
 
-// export function imageUrlFor(source) {
-//   return builder.image(source)
-// }
+export function imageUrlFor(source) {
+  return builder.image(source)
+}
 
-// const serializer = {
-//   types: {
-//     image: props => {
-//       const url = imageUrlFor(props.node.asset).url()
-//       return (
-//         <figure>
-//           <img src={url} alt={props.node.alt || 'collection Image'} />
-//         </figure>
-//       )
-//     },
-//   },
-// }
+const serializer = {
+  types: {
+    image: props => {
+      const url = imageUrlFor(props.node.asset).url()
+      return (
+        <figure>
+          <img
+            src={url}
+            alt={props.node.alt || 'collection Image'}
+            srcSet={`https://cdn.sanity.io/images/${clientConfig.sanity.projectId}/${clientConfig.sanity.dataset}/${props.node.asset._ref}`}
+          />
+        </figure>
+      )
+    },
+  },
+}
 
 const PortableText = ({ blocks }) => (
   <BasePortableText blocks={blocks} serializers={serializer} />

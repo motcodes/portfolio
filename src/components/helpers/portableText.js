44f57aeb6819e6138bce63e906df246b3fa479c6
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2021-06-05 14:35:41 +0200

    minor changes

--- src/components/helpers/portableText.js
+++ src/components/helpers/portableText.js
@@ -1,9 +1,31 @@
 import React from 'react'
 import clientConfig from '../../../client-config'
 import { default as BasePortableText } from '@sanity/block-content-to-react'
+import urlBuilder from '@sanity/image-url'
+
+const urlFor = source => urlBuilder({ ...clientConfig.sanity }).image(source)
+
+const serializer = {
+  types: {
+    mainImage: props => (
+      <figure>
+        <img
+          src={urlFor(props.node.asset)
+            .width(600)
+            .url()}
+          alt={props.node.alt}
+        />
+      </figure>
+    ),
+  },
+}
 
 const PortableText = ({ blocks }) => (
-  <BasePortableText blocks={blocks} {...clientConfig.sanity} />
+  <BasePortableText
+    blocks={blocks}
+    serializers={serializer}
+    {...clientConfig.sanity}
+  />
 )
 
 export default PortableText

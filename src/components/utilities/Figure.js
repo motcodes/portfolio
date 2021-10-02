e552cbb14009d2f200469756d742e74f88312d61
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2021-10-02 17:03:49 +0200

    minor changes

--- src/components/utilities/Figure.js
+++ src/components/utilities/Figure.js
@@ -2,7 +2,10 @@ import React from 'react'
 import ImageUrlBuilder from '@sanity/image-url'
 import clientConfig from '../../../client-config'
 
-const builder = ImageUrlBuilder(clientConfig.sanity)
+const builder = ImageUrlBuilder({
+  projectId: process.env.SANITY_PROJECT_ID,
+  dataset: process.env.SANITY_PROJECT_DATASET,
+})
 
 export function imageUrlFor(source) {
   return builder.image(source)

07c56284617a3ff177cb6cc5ae4bd22db0144a1b
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2023-03-15 14:37:50 +0100

    remove gallery for now

--- apps/portfolio/src/pages/gallery.jsx
+++ apps/portfolio/src/pages/gallery.jsx
@@ -7,32 +7,33 @@ import { Header } from '@/components/header'
 import GalleryImage from '@/components/galleryImage'
 import useDevice from '@/hooks/useDevice'
 
-const Page = ({ data, images }) => {
-  const { title } = data
-  const [_images, setImages] = useState([images])
-  const device = useDevice()
-  const size = `(max-width: 767px) 100vw,
-              (max-width: 1023px) 50vw,
-              33vw`
+// const Page = ({ data, images }) => {
+const Page = () => {
+  // const { title } = data
+  // const [_images, setImages] = useState([images])
+  // const device = useDevice()
+  // const size = `(max-width: 767px) 100vw,
+  //             (max-width: 1023px) 50vw,
+  //             33vw`
 
   // TODO: add infinite scroller to fetch images
 
-  useEffect(() => {
-    let modulo = device === 'desktop' ? 3 : device === 'tablet' ? 2 : 1
-    if (modulo > 1) {
-      let arr = Array.from(Array(modulo), () => [])
-      for (let i = 0; i < images.length; i++) {
-        const index = i % modulo
-        arr[index] = [...arr[index], images[i]]
-      }
-      setImages(arr)
-    }
-  }, [device])
+  // useEffect(() => {
+  //   let modulo = device === 'desktop' ? 3 : device === 'tablet' ? 2 : 1
+  //   if (modulo > 1) {
+  //     let arr = Array.from(Array(modulo), () => [])
+  //     for (let i = 0; i < images.length; i++) {
+  //       const index = i % modulo
+  //       arr[index] = [...arr[index], images[i]]
+  //     }
+  //     setImages(arr)
+  //   }
+  // }, [device])
 
   return (
     <>
       <main>
-        <Header title={title} />
+        <Header title={'Coming soon...'} />
         <section
           className={clsx(
             'px-3 py-12 grid grid-cols-1 gap-4 min-h-screen',
@@ -40,7 +41,8 @@ const Page = ({ data, images }) => {
             'lg:px-6 lg:p-4 lg:grid-cols-3'
           )}
         >
-          {!!_images?.length &&
+          <h1>Coming soon...</h1>
+          {/* {!!_images?.length &&
             _images.map((col, index) => (
               <div key={index} className='flex flex-col gap-4'>
                 {col.map((img) => (
@@ -70,7 +72,7 @@ const Page = ({ data, images }) => {
                   </>
                 ))}
               </div>
-            ))}
+            ))} */}
         </section>
       </main>
     </>
@@ -79,36 +81,36 @@ const Page = ({ data, images }) => {
 
 export default Page
 
-export async function getStaticProps() {
-  const data = await sanityClient.fetch(galleryQuery)
-  let _images = []
-  for (let i = 1; i <= 3; i++) {
-    const res = await fetch(
-      `https://api.unsplash.com/users/matthiasoberholzer/photos?page=${i}&client_id=7fa359ea7087a00fe9e4d1e0646ecf7314644f43bb5687aeb1454f8152893109`
-    )
-    const image = await res.json()
-    if (image[0]?.urls.regular) {
-      const _image = image.map((item) => ({
-        id: item.id,
-        blur_hash: `data:image/png;base64,${item.blur_hash}`,
-        imageSmall: item.urls.small,
-        imageRegular: item.urls.regular,
-        link: item.links.html,
-        ratio: item.width / item.height,
-      }))
-      _images = [..._images, ..._image]
-    }
-  }
+// export async function getStaticProps() {
+//   const data = await sanityClient.fetch(galleryQuery)
+//   let _images = []
+//   for (let i = 1; i <= 3; i++) {
+//     const res = await fetch(
+//       `https://api.unsplash.com/users/matthiasoberholzer/photos?page=${i}&client_id=7fa359ea7087a00fe9e4d1e0646ecf7314644f43bb5687aeb1454f8152893109`
+//     )
+//     const image = await res.json()
+//     if (image[0]?.urls.regular) {
+//       const _image = image.map((item) => ({
+//         id: item.id,
+//         blur_hash: `data:image/png;base64,${item.blur_hash}`,
+//         imageSmall: item.urls.small,
+//         imageRegular: item.urls.regular,
+//         link: item.links.html,
+//         ratio: item.width / item.height,
+//       }))
+//       _images = [..._images, ..._image]
+//     }
+//   }
 
-  const seoImage = urlForImage(data.seo.seoImage).url()
+//   const seoImage = urlForImage(data.seo.seoImage).url()
 
-  return {
-    props: {
-      data,
-      images: _images,
-      title: data.seo.seoTitle,
-      description: data.seo.seoDescription,
-      ogImage: seoImage,
-    },
-  }
-}
+//   return {
+//     props: {
+//       data,
+//       images: _images,
+//       title: data.seo.seoTitle,
+//       description: data.seo.seoDescription,
+//       ogImage: seoImage,
+//     },
+//   }
+// }

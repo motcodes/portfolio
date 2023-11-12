58fd5534e7503126fbf68436d9ea882b34f07447
Matthias Oberholzer <matthias.m.oberholzer@gmail.com>
2023-11-12 11:38:55 +0100

    feat(blockContent): allow mailto tel

--- apps/studio/schemas/objects/blockContent.js
+++ apps/studio/schemas/objects/blockContent.js
@@ -32,6 +32,10 @@ const blockContent = {
                 title: 'URL',
                 name: 'href',
                 type: 'url',
+                validation: (Rule) =>
+                  Rule.uri({
+                    scheme: ['http', 'https', 'mailto', 'tel'],
+                  }),
               },
             ],
           },
